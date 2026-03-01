91c2276e4135bc55a3b5d05a1251ba00a23fc330
Shaghik66 <kaprielianshaghig961@gmail.com>
2026-03-01 09:59:58 -0800

    Slider code editing

--- src/components/HeaderSlider/HeaderSlider.jsx
+++ src/components/HeaderSlider/HeaderSlider.jsx
@@ -3,17 +3,12 @@ import React, { useState } from "react";
 
 export default function HeaderSlider({ sliderImages }) {
   let [slider, setSlider] = useState(0);
-  let [sliderImage, setSliderImage] = useState(sliderImages[slider]);
 
   let increment = () => {
-    setSliderImage(sliderImages[slider]);
-
     sliderImages.length - 1 == slider ? setSlider(0) : setSlider(slider + 1);
   };
 
   let decrement = () => {
-    setSliderImage(sliderImages[slider]);
-
     slider == 0 ? setSlider(sliderImages.length - 1) : setSlider(slider - 1);
   };
 
@@ -26,7 +21,11 @@ export default function HeaderSlider({ sliderImages }) {
           alt="Left icon png"
         />
       </button>
-      <img className="sliderImage" src={sliderImage} alt="Slider Image" />
+      <img
+        className="sliderImage"
+        src={sliderImages[slider]}
+        alt="Slider Image"
+      />
       <button className="sliderButton rightButton" onClick={increment}>
         <img
           className="buttonIcon "
